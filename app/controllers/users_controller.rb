9af385acbc2f0ec1d8b475e5d01a6af2686915b4
Alexandro Alanya <alexandro.alanya@gmail.com>
2019-07-02 18:35:07 -0400

    Adds User creaate form  Please enter the commit message for your changes. Lines starting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,22 @@ class UsersController < ActionController::Base
   def new
     @user = User.new
     render :new
-
   end
 
   def create
+    @users= User.create(user_params)
+
+    redirect_to "/users/#{@users.id}"
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :age, :gender, :is_captain, :team_id)
   end
 end
